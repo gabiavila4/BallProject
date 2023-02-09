167ba30b6cdb7562aabb4c928a7fb2ba3617a43e
gabriela.avila24 <gabrielanicoleavila@icloud.com>
2023-02-09 11:29:47 -0600

    initial commit working 20 ball

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -6,12 +6,14 @@ public class DrawFrame
     public static void main(String[] args)
     {
 
+        JButton myButton = new JButton();
         JFrame myFrame = new JFrame();
         myFrame.setSize(500, 500);
         drawPanel myPanel = new drawPanel();
         BallPanel myBPanel = new BallPanel();
         // Ball myB = new Ball();
 
+        myBPanel.add(myButton);
         myFrame.add(myBPanel);
         myFrame.setVisible(true);
     }
